55bc043c97b20f25e22b62962e79b81a01fdedcd
VitalieLozan <vitalielozan17@outlook.com>
2025-06-17 11:21:14 +0200

    added some image

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { FcPrevious, FcNext } from "react-icons/fc";
+import { Button, Image } from "@heroui/react";
 
 function ImageCarousel({ srcArray = [] }) {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
@@ -19,29 +20,32 @@ function ImageCarousel({ srcArray = [] }) {
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex">
           {srcArray.map((src, idx) => (
-            <div className="min-w-full flex-shrink-0" key={idx}>
-              <img
+            <div
+              className="flex min-w-full flex-shrink-0 items-center justify-center"
+              key={idx}
+            >
+              <Image
                 src={src}
                 alt={`Slide ${idx}`}
-                className="h-64 w-full rounded-xl object-cover"
+                className="h-64 max-w-[90%] rounded-xl object-cover"
               />
             </div>
           ))}
         </div>
       </div>
 
-      <button
+      <Button
         onClick={scrollPrev}
-        className="absolute left-2 top-1/2 -translate-y-1/2 rounded-full bg-white/80 p-2 text-gray-700 shadow-md hover:bg-white dark:bg-gray-800 dark:text-white"
+        className="absolute left-2 top-1/2 -translate-y-1/2 rounded-full bg-transparent p-2 text-gray-700 shadow-sm dark:text-white"
       >
         <FcPrevious />
-      </button>
-      <button
+      </Button>
+      <Button
         onClick={scrollNext}
-        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full bg-white/80 p-2 text-gray-700 shadow-md hover:bg-white dark:bg-gray-800 dark:text-white"
+        className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full bg-transparent p-2 text-gray-700 shadow-sm dark:text-white"
       >
         <FcNext />
-      </button>
+      </Button>
     </div>
   );
 }
